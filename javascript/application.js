60c21213520c3b534a0e43c4481f41932d1ead93
Paula Brouwer <freelance.brouwer@gmail.com>
2023-08-31 11:58:13 +0200

    Add a reset link

--- javascript/application.js
+++ javascript/application.js
@@ -5,30 +5,28 @@ window.Stimulus = Application.start()
 Stimulus.register("disable-button", DisableButtonController)
 
 
+// // by class
+// const button = document.querySelector('.btn')
+// console.log(button)
+// const element1 = document.getElementsByClassName('btn')[0]
+// console.log(element1)
 
+// // by id
+// const element2 = document.querySelector('#click-me')
+// console.log(element2)
+// const element3 = document.getElementById('click-me')
+// console.log(element3)
 
-// by class
-const button = document.querySelector('.btn')
-console.log(button)
-const element1 = document.getElementsByClassName('btn')[0]
-console.log(element1)
+// // Vanilla JavaScript
 
-// by id
-const element2 = document.querySelector('#click-me')
-console.log(element2)
-const element3 = document.getElementById('click-me')
-console.log(element3)
+// // anonymous function
+// button.addEventListener("click", (element) => {
+//   console.log("I was clicked")
+// })
 
-// Vanilla JavaScript
-
-// anonymous function
-button.addEventListener("click", (element) => {
-  console.log("I was clicked")
-})
-
-// named function
-const buttonClickAction =  (event) => {
-  event.currentTarget.classList.remove('btn-primary')
-  event.currentTarget.innerText = "Bingo!"
-}
-button.addEventListener("click", buttonClickAction)
+// // named function
+// const buttonClickAction =  (event) => {
+//   event.currentTarget.classList.remove('btn-primary')
+//   event.currentTarget.innerText = "Bingo!"
+// }
+// button.addEventListener("click", buttonClickAction)
--- javascript/controllers/disable_button_controller.js
+++ javascript/controllers/disable_button_controller.js
@@ -2,13 +2,23 @@ import { Controller } from "stimulus";
 
 
 export default class extends Controller {
+  static targets = ["button", "link"]
+
   connect(){
     console.log("Hello")
+    console.log("link", this.linkTarget)
   }
 
   disable(){
     console.log("Disable button pressed")
-    this.element.innerText = "Bingo"
-    this.element.setAttribute("disabled", " ")
+    this.buttonTarget.innerText = "Bingo"
+    this.buttonTarget.setAttribute("disabled", " ")
+    this.linkTarget.classList.remove('d-none')
+  }
+
+  reset(){
+    this.buttonTarget.innerText = "Click me!"
+    this.buttonTarget.removeAttribute("disabled")
+    this.linkTarget.classList.add('d-none')
   }
 }
