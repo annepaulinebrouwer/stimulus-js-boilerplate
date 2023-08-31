f940abbb8b85e2e62faa44b785762edda5d7825b
Paula Brouwer <freelance.brouwer@gmail.com>
2023-08-31 11:05:58 +0200

    Playing around with Vanilla JavaScript

--- javascript/application.js
+++ javascript/application.js
@@ -1,2 +1,28 @@
 import { Application, Controller } from 'stimulus';
 window.Stimulus = Application.start()
+
+// by class
+const button = document.querySelector('.btn')
+console.log(button)
+const element1 = document.getElementsByClassName('btn')[0]
+console.log(element1)
+
+// by id
+const element2 = document.querySelector('#click-me')
+console.log(element2)
+const element3 = document.getElementById('click-me')
+console.log(element3)
+
+// Vanilla JavaScript
+
+// anonymous function
+button.addEventListener("click", (element) => {
+  console.log("I was clicked")
+})
+
+// named function
+const buttonClickAction =  (event) => {
+  event.currentTarget.classList.remove('btn-primary')
+  event.currentTarget.innerText = "Bingo!"
+}
+button.addEventListener("click", buttonClickAction)
